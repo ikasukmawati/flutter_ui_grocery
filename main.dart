4365c459df95d2944cb73a0d54244c03e4e81082
ika sukmawati <103030263+ikasukmawati@users.noreply.github.com>
2023-11-30 09:12:54 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:grocery/data.dart';
+import 'package:grocery/item_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  final data = Product('bayam', '2.000', '1ikat', 'assets/img1.png', 'secara umum sayuran dan buah-buahan merupakan sumber berbagai vitamin, mineral');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.black,
+        ),
+        title: const Text(
+          "Toko Buah & Sayur",
+          style: TextStyle(color: Colors.black)
+        ),
+        actions: [
+          Row(
+            children: [
+              const Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              Stack(
+                children: [
+                  IconButton(
+                      onPressed: (() {}),
+                      icon: const Icon(
+                        Icons.shopping_cart,
+                        color: Colors.black,
+                      )
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 3,
+                    child: Container(
+                      height: 20,
+                      width: 20,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
+                      ),
+                      child: Text(
+                        "2",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ),
+
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.6,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+          ),
+          itemBuilder: (context,index){
+            return ItemWidget(product: allData[index]);
+          },
+          itemCount: allData.length,
+
+        ),
+      ),
+    );
+  }
+}
+
+// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
+
+
